f10578a130ccffb91aa03bde0b96dd41749d62b3
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2022-12-01 01:52:07 -0500

    setup: add cryptography

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.0.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["ipykernel", "pytest"],
+    install_requires=["ipykernel", "pytest", "cryptography"],
 )
