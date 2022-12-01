400ea5e9c760c416abfd3b94190065fd1cf6edf3
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2022-12-01 01:40:52 -0500

    setup: create

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hacc",
+    version="0.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["ipykernel", "pytest"],
+)
